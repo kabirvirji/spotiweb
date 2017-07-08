ba28a278dd2669a2e8d1eb6f9c4381580b47b7cf
kabirvirji <kabirvirji@gmail.com>
2017-07-08 12:25:41 -0700

    trigger when f8 is pressed

---
+++ index.js
@@ -0,0 +1,27 @@
+var keypress = require('keypress')
+  , tty = require('tty');
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+  // when f8 pressed
+  if (key.name == 'f8') {
+    console.log("you pressed f8")
+  }
+});
+
+if (typeof process.stdin.setRawMode == 'function') {
+  process.stdin.setRawMode(true);
+} else {
+  tty.setRawMode(true);
+}
+process.stdin.resume();
+
+//\u001b[19~ sequence
+//[19~ code
